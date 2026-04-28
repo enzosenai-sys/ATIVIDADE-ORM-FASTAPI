c2c5198775e6512844123e4afebb45b873814eeb
henzo <enzon3ko@gmail.com>
2026-04-28 15:47:44 -0300

    add novidades

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
-from sqlalchemy import create_engine, Column, Integer, String, Foreignkey
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy import Float
+
+
+    
 
 #Banco
 engine = create_engine("sqlite:///./lojadb", connect_args={"check_same_thread": False})
@@ -15,6 +19,7 @@ class Categoria(Base):
     __tablename__ = "categorias"
     id = Column(Integer, primary_key=True)
     nome = Column(String)
+    descricao = Column(String)
 
     produtos = relationship("Produto", back_populates="categoria")
 
@@ -23,9 +28,11 @@ class Produto(Base):
     __tablename__ = "produtos"
     id = Column(Integer, primary_key=True)
     nome = Column(String)
+    preco = Column(Float, nullable=False)
+    estoque = Column(Integer, nullable=False)
 
-    categoria_id = Column(Integer, Foreignkey("categorias.id"))
-    categoria = relatioship("Categoria", back_populates="produtos")
+    categoria_id = Column(Integer, ForeignKey("categorias.id"))
+    categoria = relationship("Categoria", back_populates="produtos")
 
 Base.metadata.create_all(bind=engine)
 
@@ -50,9 +57,9 @@ def listar_categorias(request: Request):
 
     })
 @app.post("/categorias")
-def criar_categoria(nme: str = Form(...)):
+def criar_categoria(nome: str = Form(None)):
     db = SessionLocal()
-    db.add(Categoria(nome=Categoria))
+    db.add(Categoria(nome=nome, descricao=declarative_base))
     db.commit()
     db.close()
     return RedirectResponse("/categorias", status_code=303)
@@ -78,3 +85,4 @@ def criar_produto(nome: str = Form(...), categoria_id: int = Form(...)):
     db.commit()
     db.close()
     return RedirectResponse("/produtos", status_code=303)
+
